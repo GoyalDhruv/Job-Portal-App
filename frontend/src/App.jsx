65e5f9b58e9ffd5af53de6a8035473bd9429b750
dhruv <dhruvgoyal014@gmail.com>
2024-09-25 19:07:42 +0530

    lazy loader added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,43 +1,40 @@
-import { useState } from 'react'
-import './App.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Login from './components/auth/Login'
-import Signup from './components/auth/Signup'
-import Home from './components/Home/Home'
-import Jobs from './components/Jobs/Jobs'
-import Browse from './components/Browse/Browse'
+import { Suspense, lazy } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+import PropTypes from 'prop-types';
+import { RotateLoader } from 'react-spinners';
 
+const Home = lazy(() => import('./components/Home/Home'));
+const Login = lazy(() => import('./components/auth/Login'));
+const Signup = lazy(() => import('./components/auth/Signup'));
+const Jobs = lazy(() => import('./components/Jobs/Jobs'));
+const Browse = lazy(() => import('./components/Browse/Browse'));
 
-const appRouter = createBrowserRouter([
-  {
-    path: '/',
-    element: <Home />
-  },
-  {
-    path: '/login',
-    element: <Login />
-  },
-  {
-    path: '/signup',
-    element: <Signup />
-  },
-  {
-    path: '/jobs',
-    element: <Jobs />
-  },
-  {
-    path: '/browse',
-    element: <Browse />
-  }
-])
+const LazyRoute = ({ element }) => (
+  <Suspense fallback={
+    <div className='flex justify-center items-center h-screen'>
+      <RotateLoader color='#F83002' />
+    </div>
+  }>
+    {element}
+  </Suspense>
+);
 
 function App() {
-
   return (
-    <>
-      <RouterProvider router={appRouter} />
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<LazyRoute element={<Home />} />} />
+        <Route path="/login" element={<LazyRoute element={<Login />} />} />
+        <Route path="/signup" element={<LazyRoute element={<Signup />} />} />
+        <Route path="/jobs" element={<LazyRoute element={<Jobs />} />} />
+        <Route path="/browse" element={<LazyRoute element={<Browse />} />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+LazyRoute.propTypes = {
+  element: PropTypes.node.isRequired,
+};
+export default App;
